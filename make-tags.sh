84b7825e54f51de5baeb0b8e46a50e27157da014
rhradec <me@hradec.com>
2019-08-14 11:55:08 -0700

    added some info feedback

--- make-tags.sh
+++ make-tags.sh
@@ -6,6 +6,7 @@ cd $CD/pipeline/tags/
 # loop over tags and creates the tag folders pulling the tar file from the github releases
 git tag | while read tag ; do 
 	echo $tag
+	echo "https://github.com/hradec/pipeVFX/archive/$tag.tar.gz"
 	curl -L "https://github.com/hradec/pipeVFX/archive/$tag.tar.gz" | \
 		tar xzv --exclude="golaem" --xform="s/pipeVFX-......pipeline.tools/$tag/x"  pipeVFX-$tag/pipeline/tools
 done
