b321f4c80d9f63ccb56f78dfa2a0acc8a343fbd0
rhradec <me@hradec.com>
2019-08-19 11:51:10 -0700

    added a fix to load a hand-built glibc and libXft when running on centos 6 (nuke needs newer glibc than centos 6 has)

--- pipeline/tools/python/pipe/baseApp.py
+++ pipeline/tools/python/pipe/baseApp.py
@@ -1320,6 +1320,17 @@ class baseApp(_environ):
         if hasattr( self, 'preRun'):
             cmd = self.preRun(cmd)
 
+        # workaround for centos 6.5
+        if os.path.exists('/etc/centos-release'):
+            preload = [
+                "/opt/glibc-2.14/lib/libc.so.6",
+                "/opt/centos7/usr/lib64/libXft.so.2",
+            ]
+            if 'LD_PRELOAD' in os.environ.keys():
+                preload += [os.environ['LD_PRELOAD']]
+
+            os.environ['LD_PRELOAD']=":".join(preload)
+
         log.debug(cmd)
 
         # remove TBB_VERSION to avoid TBB spitting out information!!
