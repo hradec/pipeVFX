158f73991a26b29e3e6f5102ec188577aa81b7b2
rhradec <me@hradec.com>
2019-09-02 20:09:35 -0700

    fixed to create the proper tag folders. Also uploads to server now.

--- make-tags.sh
+++ make-tags.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+server=192.168.0.12
+
 CD=$(dirname $(readlink -f $BASH_SOURCE))
 cd $CD/pipeline/tags/
 
 # loop over tags and creates the tag folders pulling the tar file from the github releases
-git tag | while read tag ; do 
+git tag | while read tag ; do
 	echo $tag
 	echo "https://github.com/hradec/pipeVFX/archive/$tag.tar.gz"
 	if [ ! -e $tag ] ; then
 		curl -L "https://github.com/hradec/pipeVFX/archive/$tag.tar.gz" | \
 			tar xzv --exclude="golaem" --xform="s/pipeVFX-......pipeline.tools/$tag/x"  pipeVFX-$tag/pipeline/tools
+			mv pipeVFX-$tag/pipeline/tools $tag
+			rm -rf pipeVFX-$tag
+
+			# upload to server
+			sudo rsync -avpP ./$tag/ $server:/atomo/pipeline/tags/$tag/
 	fi
 done
-
