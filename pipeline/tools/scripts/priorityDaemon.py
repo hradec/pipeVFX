839b58487c413c6fcc26c55fc026e162572a4fd9
rhradec <me@hradec.com>
2019-10-09 02:49:50 -0700

    adjustments to priority - last one was killing the file server.

--- pipeline/tools/scripts/priorityDaemon.py
+++ pipeline/tools/scripts/priorityDaemon.py
@@ -10,33 +10,31 @@ while True:
             cmdline = ' '.join(p.cmdline())
             # if process is prman
             if 'bin/prman' in cmdline or 'bin/maya_prman' in cmdline :
-                # print cmdline
                 if 'bin/it' not in cmdline:
                     # if not running interactively (running in the farm)
                     # use the lowest priority so to not interferir with
                     # people working
-                    if p.nice() != 19:
-                        p.nice(19)
-                        p.ionice(psutil.IOPRIO_CLASS_IDLE)
+                    p.nice(19)
+                    p.ionice(psutil.IOPRIO_CLASS_IDLE)
                 else:
                     # if running interactively, set to normal priority
                     # since it can slow down IT feedback when using a large
                     # number of cores.
-                    if p.nice() != 10:
-                        p.nice(0)
+                    p.nice(0)
 
             # or if its IT
             elif [ x for x in ['bin/it'] if x in cmdline ]:
-                # print cmdline
-                if p.nice() != -18:
-                    p.nice(-18)
-                    p.ionice(psutil.IOPRIO_CLASS_RT)
+                p.nice(-16)
+                p.ionice(psutil.IOPRIO_CLASS_BE)
 
             # or if its xpra or ssh
             elif [ x for x in ['xpra', 'xvfb', 'ssh'] if x in cmdline ]:
-                # print cmdline
                 p.nice(-19)
-                p.ionice(psutil.IOPRIO_CLASS_RT)
+                p.ionice(psutil.IOPRIO_CLASS_BE)
+
+            elif [ x for x in ['chrome'] if x in cmdline ]:
+                p.nice(19)
+                p.ionice(psutil.IOPRIO_CLASS_IDLE)
 
         except:
             pass
