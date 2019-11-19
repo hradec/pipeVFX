be94f510ba7e7a824f9d1a5a58abc84ef1607683
rhradec <me@hradec.com>
2019-11-19 02:14:17 -0800

    run scons in parallel when building in CI

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,11 @@
 env
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 cd /atomo/pipeline/build/
-[ "$TRAVIS" == "1" ] && rm -rf "/atomo/pipeline/libs/*"
+
+if [ "$TRAVIS" == "1" ] ; then
+    rm -rf "/atomo/pipeline/libs/*"
+    EXTRA="-j 8 $EXTRA"
+fi
 mkdir -p /atomo/pipeline/libs/
 
 export TERM=xterm-256color
