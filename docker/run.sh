f11f0b23b0c60a0bcf726110285c8c0dc5032d99
rhradec <me@hradec.com>
2019-12-10 01:11:54 -0800

    running the build 3 times, since it seems sometimes a package fails but builds on a second try, when using cuda!

--- docker/run.sh
+++ docker/run.sh
@@ -15,5 +15,11 @@ export TERM=xterm-256color
 if [ "$RUN_SHELL" == "1" ] ; then
     bash --init-file /atomo/pipeline/tools/init/bash -i
 else
-    /usr/bin/nice -n 19 scons install $EXTRA $DEBUG
+    # we run scons 3 times just in case, since pkgs using
+    # cuda may fail to build at first, but build on a second try.
+    # also, this accounts for the 2 stage build, when initially there's
+    # no libs so we can't build any app dependent package, like cortex
+    for n in {1..3} ; do
+        /usr/bin/nice -n 19 scons install $EXTRA $DEBUG
+    done
 fi
