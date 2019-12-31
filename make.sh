ade099b44bc3524cf38cb9910a3914b5981ce5d3
rhradec <me@hradec.com>
2019-12-30 17:07:06 -0800

    now 'make.sh -s' runs under the host user, with sudo rights inside docker, and running dbus service to properly create folders

--- make.sh
+++ make.sh
@@ -148,21 +148,34 @@ else
     X11=""
     if [ "$SHELL" == "1" ] ; then
         X11=" -v /tmp/.X0-lock:/tmp/.X0-lock:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e QT_X11_NO_MITSHM=1 -e DISPLAY -e XAUTHORITY  "
-        # X11="$X11 -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v /etc/shadow:/etc/shadow:ro -v /etc/sudoers.d:/etc/sudoers.d:ro --user=$USER "
-        if [ "$(which nvidia-smi)" != "" ] ; then
-            # X11="$X11 --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=all "
-            extra_libs1=$(ldconfig -p | grep libGL | grep -v GLU |  grep x86 | awk  '{print $NF}' | while read p ; do [ "$p" != "" ] && pp=$(readlink -f $p) && echo -v $pp:/lib64/$(basename $p):ro ; done)
-            extra_libs2=$(ldconfig -p | grep libnvidia | grep 440.36 | grep x86 | awk  '{print "-v "$NF":/lib64/"$1":ro"}')
-            # extra_libs3=$(ldconfig -p | grep libstdc++ |grep x86 | awk  '{print "-v "$NF":/lib64/"$1":ro"}')
-            # extra_libs4=$(ldconfig -p | grep libgcc_s |grep x86 | awk  '{print "-v "$NF":/lib64/"$1":ro"}')
-            X11=" $X11 $extra_libs1 $extra_libs2 $extra_libs3 $extra_libs4 "
-            # X11=" $X11 --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket "
-        fi
+        # if [ "$(which nvidia-smi)" != "" ] ; then
+        #     X11="$X11 --runtime=nvidia -e NVIDIA_DRIVER_CAPABILITIES=all "
+        # fi
+
+        # instead of using just nvidia custom runtime, we choosed to use the host systems
+        # GL libraries, by mounting then to the docker image. This way, the docker
+        # image will use libraries compatible with the host video driver, no matter
+        # what video driver/manufacturer it is!
+        # this seems to work fine with NVidia drivers on arch linux!
+        # need more testing on other distros/video board setups.
+        # this mode only works if we run the docker container with --privileged!
+        extra_libs1=$(ldconfig -p | grep libGL | grep -v GLU |  grep x86 | awk  '{print $NF}' | while read p ; do [ "$p" != "" ] && pp=$(readlink -f $p) && echo -v $pp:/lib64/$(basename $p):ro ; done)
+        extra_libs2=$(ldconfig -p | grep libnvidia | grep 440.36 | grep x86 | awk  '{print "-v "$NF":/lib64/"$1":ro"}')
+        X11=" $X11 $extra_libs1 $extra_libs2 $extra_libs3 $extra_libs4 "
+        # X11=" $X11 --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket "
+
+        # and let X accept connections no matter what
+        xhost +
     fi
 
     # create lib folder
     mkdir -p "$CD/pipeline/libs/"
     mkdir -p "$CD/pipeline/build/.build/"
+
+    # this one liner creates the _uid, _user, _gid and _group env vars, so we can
+    # pass it on to the docker container.
+    # as it uses the id command, it should work on any host distro that has id command
+    eval $(echo $(for n in $(id) ; do echo $n | tr '()' ' ' | egrep 'gid|uid' ; done  | awk -F'=' '{print $2}') | awk '{print "export _uid="$1,"; export _user="$2,"; export _gid="$3,"; export _group="$4}')
 
     # now we can finally run a build!
     cmd="docker rm -f pipevfx_make >/dev/null 2>&1 ; \
@@ -174,7 +187,12 @@ else
         -v $CD/pipeline/build/.build/:/atomo/pipeline/build/.build/ \
         -v $CD/docker/run.sh:/run.sh \
         -v $CD/.root:/atomo/.root \
+        -v $HOME:/home/$USER/ \
         $APPS_MOUNT \
+        -e _UID=$_uid \
+        -e _USER=$_user \
+        -e _GID=$_gid \
+        -e _GROUP=$_group \
         -e RUN_SHELL=$SHELL \
         -e EXTRA=\"$EXTRA\" \
         -e DEBUG=\"$DEBUG\" \
@@ -187,6 +205,6 @@ else
         --privileged \
         $build_image"
 
-    echo $cmd
+    # echo $cmd
     eval $cmd
 fi
