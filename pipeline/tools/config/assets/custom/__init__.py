171313574532f8c02aaf657ba519910792c0316c
rhradec <me@hradec.com>
2020-04-19 02:37:03 -0700

    unificating all code

--- pipeline/tools/config/assets/custom/__init__.py
+++ pipeline/tools/config/assets/custom/__init__.py
@@ -65,6 +65,7 @@ def checkoutRigAndApplyAnimation(self, paths = None):
             # if m.ls('|%s*' % GRP):
             #     m.delete('|%s*' % GRP)
 
+
             # now we import the file, asset and attach the controls.
             pb1 = genericAsset.progressBar(len(paths)*3+1, 'importing assets and animation from maya scene file...')
             pb1.step()
@@ -82,54 +83,55 @@ def checkoutRigAndApplyAnimation(self, paths = None):
 
                 m.setAttr('%s.visibility' % GRP_asset_maya_name, 0)
 
-                # leave only the controls from the imported scene
-                # and gather their names into a list
-                pb1.step()
-                m.delete([ x for x in m.ls('%s|*' % GRP_asset_maya_name, dag=1, l=1, type='mesh') if CTRLS not in x ])
-                CTRLS_MESHES = [ x for x in m.ls('%s|*' % GRP_asset_maya_name, dag=1, l=1, type='mesh') if CTRLS in x ]
-                CTRLS_MESHES.sort()
-                CTRLS_MESHES = list(set(CTRLS_MESHES))
-
-                # attach the meshes from the imported ctrls to the asset rig ones
-                # gather the controls from the imported sam asset
-                asset_nodes = [ x for x in m.ls( '|SAM_%s*' % asset_maya_name, dag=1, l=1 ) if CTRLS in x and GRP not in x ]
-                asset_nodes.sort()
-                asset_nodes = list(set(asset_nodes))
-                # now go over the animated crtl nodes and attach then to
-                # the asset rig crtl nodes!
-                pb2 = genericAsset.progressBar(len(CTRLS_MESHES)+1, 'Attaching animation controls to imported rig assets...')
-                pb2.step()
-                md5 = nodeMD5.nodeMD5()
-                for node in CTRLS_MESHES:
-                    pb2.step()
-                    tnode = m.listRelatives(node, p=1, f=1)[0]
-                    anim = node.split(ROOT)[-1]
-
-                    # use the embbed md5 on the animation to find the nodes we
-                    # have to attach to!
-                    nodemd5 = md5.getNodeMD5(node)
-                    if nodemd5:
-                        nodes_original_rig = [ x for x in md5.getNodeByMD5( nodemd5 ) if x != node and asset_maya_name in x ]
-                    else:
-                        nodes_original_rig = [ x for x in asset_nodes if anim in x ]
-
-                    nodes_original_rig.sort()
-                    nodes_original_rig = list(set(nodes_original_rig))
-                    # only select the asset rig crtl nodes that match the
-                    # current animated one!
-                    for rig in nodes_original_rig:
-                        trig  = m.listRelatives(rig, p=1, f=1)[0]
-                        # print asset_maya_name
-                        # print tnode
-                        # print trig
-                        # sys.stdout.flush()
-                        if m.nodeType( node ) == 'mesh':
-                            m.connectAttr( tnode+".translate", trig+".translate", f=1 )
-                            m.connectAttr( tnode+".rotate", trig+".rotate", f=1 )
-                            m.connectAttr( tnode+".scale", trig+".scale", f=1 )
-
-                pb2.close()
-            pb1.close()
+            assetListAssetImport(self)
+            #     # leave only the controls from the imported scene
+            #     # and gather their names into a list
+            #     pb1.step()
+            #     m.delete([ x for x in m.ls('%s|*' % GRP_asset_maya_name, dag=1, l=1, type='mesh') if CTRLS not in x ])
+            #     CTRLS_MESHES = [ x for x in m.ls('%s|*' % GRP_asset_maya_name, dag=1, l=1, type='mesh') if CTRLS in x ]
+            #     CTRLS_MESHES.sort()
+            #     CTRLS_MESHES = list(set(CTRLS_MESHES))
+            #
+            #     # attach the meshes from the imported ctrls to the asset rig ones
+            #     # gather the controls from the imported sam asset
+            #     asset_nodes = [ x for x in m.ls( '|SAM_%s*' % asset_maya_name, dag=1, l=1 ) if CTRLS in x and GRP not in x ]
+            #     asset_nodes.sort()
+            #     asset_nodes = list(set(asset_nodes))
+            #     # now go over the animated crtl nodes and attach then to
+            #     # the asset rig crtl nodes!
+            #     pb2 = genericAsset.progressBar(len(CTRLS_MESHES)+1, 'Attaching animation controls to imported rig assets...')
+            #     pb2.step()
+            #     md5 = nodeMD5.nodeMD5()
+            #     for node in CTRLS_MESHES:
+            #         pb2.step()
+            #         tnode = m.listRelatives(node, p=1, f=1)[0]
+            #         anim = node.split(ROOT)[-1]
+            #
+            #         # use the embbed md5 on the animation to find the nodes we
+            #         # have to attach to!
+            #         nodemd5 = md5.getNodeMD5(node)
+            #         if nodemd5:
+            #             nodes_original_rig = [ x for x in md5.getNodeByMD5( nodemd5 ) if x != node and asset_maya_name in x ]
+            #         else:
+            #             nodes_original_rig = [ x for x in asset_nodes if anim in x ]
+            #
+            #         nodes_original_rig.sort()
+            #         nodes_original_rig = list(set(nodes_original_rig))
+            #         # only select the asset rig crtl nodes that match the
+            #         # current animated one!
+            #         for rig in nodes_original_rig:
+            #             trig  = m.listRelatives(rig, p=1, f=1)[0]
+            #             # print asset_maya_name
+            #             # print tnode
+            #             # print trig
+            #             # sys.stdout.flush()
+            #             if m.nodeType( node ) == 'mesh':
+            #                 m.connectAttr( tnode+".translate", trig+".translate", f=1 )
+            #                 m.connectAttr( tnode+".rotate", trig+".rotate", f=1 )
+            #                 m.connectAttr( tnode+".scale", trig+".scale", f=1 )
+            #
+            #     pb2.close()
+            # pb1.close()
 
 
 
