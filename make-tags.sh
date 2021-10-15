bc729ab7f33d8c2b37d528b88852367cb2accab0
rhradec <me@hradec.com>
2021-10-15 10:30:41 -0700

    fixed make-tags.sh to fetch the tags before creating then. Also disabled upload for now.

--- make-tags.sh
+++ make-tags.sh
@@ -7,6 +7,7 @@ mkdir -p $CD/pipeline/tags/
 cd $CD/pipeline/tags/
 
 # loop over tags and creates the tag folders pulling the tar file from the github releases
+git fetch
 git tag | while read tag ; do
 	echo $tag
 	echo "https://github.com/hradec/pipeVFX/archive/$tag.tar.gz"
@@ -18,9 +19,9 @@ git tag | while read tag ; do
 			rm -rf pipeVFX-$tag
 
 			# upload to server
-			sudo rsync -avpP ./$tag/ $server:/atomo/pipeline/tags/$tag/
-			ssh $server " cd /atomo/pipeline/tags/ ; sudo rm -f latest ; sudo ln -s $(ls | egrep '.*\..*\..*' |  sort  -V | tail -1) latest "
-			ssh $server "sudo rsync -avpP --delete /atomo/pipeline/tags/$tag/ /NAS/atomo/pipeline/tags/$tag/"
+#			sudo rsync -avpP ./$tag/ $server:/atomo/pipeline/tags/$tag/
+#			ssh $server " cd /atomo/pipeline/tags/ ; sudo rm -f latest ; sudo ln -s $(ls | egrep '.*\..*\..*' |  sort  -V | tail -1) latest "
+#			ssh $server "sudo rsync -avpP --delete /atomo/pipeline/tags/$tag/ /NAS/atomo/pipeline/tags/$tag/"
 		fi
 	fi
 done
