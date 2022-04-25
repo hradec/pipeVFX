c226815b79fdf4f35e4a97c3e98ddd84d4e8ad62
rhradec <me@hradec.com>
2022-04-25 00:44:30 -0700

    pipelineStartup now works correctly with Maya 2022 and without renderman

--- pipeline/tools/maya/scripts/pipelineStartup.py
+++ pipeline/tools/maya/scripts/pipelineStartup.py
@@ -54,24 +54,31 @@ def prependPythonPath( libs ):
         for x in p:
             sys.path.insert( 0, os.path.expandvars(x) )
 
-# if float(os.environ["MAYA_VERSION"]) > 2017:
-#     prependPythonPath(pipe.libs.pyilmbase())
-#     if not m.about(batch=1):
-#         import imath
-
-
-# now we add maya pythonpath to the top!
 if float(os.environ["MAYA_VERSION"]) > 2017:
+    import imath
+    import alembic
+    import IECore
+    import Gaffer
+    # now we add maya pythonpath to the top!
     prependPythonPath(pipe.apps.maya())
+
+    # prependPythonPath([
+    #     pipe.libs.pyilmbase(),
+    #     pipe.libs.alembic(),
+    #     pipe.libs.openvdb(),
+    #     pipe.libs.usd(),
+    # ])
+    # import imath
+    # import alembic
+    # import IECore
+    # import Gaffer
+
     if not m.about(batch=1):
         # force PySide2 to load from maya folder!
         import PySide2
         import shiboken2
         reload(PySide2)
         reload(shiboken2)
-
-    # prependPythonPath(pipe.libs.alembic())
-    # import alembic
 
 
 
@@ -86,18 +93,6 @@ startTime = time()
 
 print 'Pipeline Startup...'
 sys.stdout.flush()
-
-
-
-
-#
-if 'MAYA_USE_VRAY' in os.environ:
-    from glob import glob
-    path = "%s/autodesk/maya%s/vray/scripts/*.mel" % (pipe.apps.vray().path(), os.environ['MAYA_VERSION'])
-    for each in glob( path ):
-        try:
-            meval( 'source "%s";' % each )
-        except: pass
 
 # pipeline startup!
 def pipeIdleStartup():
@@ -117,24 +112,26 @@ def pipeIdleStartup():
         user = j.shot.user()
         m.workspace( user.path('maya'), o=1 )
 
-
     plugs=[]
-    if float(os.environ["MAYA_VERSION"]) > 2017:
-        prependPythonPath([
-            pipe.libs.pyilmbase(),
-            pipe.libs.alembic(),
-            pipe.libs.openvdb(),
-            pipe.libs.usd(),
-        ])
-        import imath
-        import alembic
-        plugs += ['RenderMan_for_Maya.py']
-    else:
-        plugs += ['RenderMan_for_Maya']
+    if pipe.isEnable('prman'):
+        if float(os.environ["MAYA_VERSION"]) > 2017:
+            plugs += ['RenderMan_for_Maya.py']
+        else:
+            plugs += ['RenderMan_for_Maya']
 
-    # if user is 3d, add qube ui!
-    # if os.environ['USER'] == '3d':
-    #     meval('qube_addUI();')
+    if pipe.isEnable('houdini'):
+        plugs += ['houdiniEngine']
+
+    if pipe.isEnable('arnold'):
+        plugs += ['mtoa']
+
+    if pipe.isEnable('vray'):
+        from glob import glob
+        path = "%s/autodesk/maya%s/vray/scripts/*.mel" % (pipe.apps.vray().path(), os.environ['MAYA_VERSION'])
+        for each in glob( path ):
+            try:
+                meval( 'source "%s";' % each )
+            except: pass
 
     # force auto-load of these plugins at startup!
     plugs+=[
@@ -143,9 +140,8 @@ def pipeIdleStartup():
         'AbcExport',
         'gpuCache',
         'ieCore',
-        'houdiniEngine',
         'OpenVDB',
-#        '3delight_for_maya%s' % os.environ['MAYA_VERSION_MAJOR'],
+        # '3delight_for_maya%s' % os.environ['MAYA_VERSION_MAJOR'],
     ]
 
     if plugs:
@@ -272,24 +268,9 @@ def loadAssetManager():
     if IECore :
         def __publishRender() :
             __publish('render','maya')
-            # import maya.cmds as m
-            # #m.file()
-            #
-            # import IECore
-            # import Gaffer
-            # import GafferUI
-            # import os, pipe
-            # os.environ['PIPE_PUBLISH_FILTER'] = 'render/maya'
-            # appLoader = IECore.ClassLoader.defaultLoader( "GAFFER_APP_PATHS" )
-            # appLoader.classNames()
-            # app=appLoader.load( 'opa' )()
-            # app.parameters()['arguments'] = IECore.StringVectorData(['-Asset.type','render/maya','1','IECORE_ASSET_OP_PATHS'])
-            # app.parameters()['op'] = 'publish'
-            # app.parameters()['gui'] = 1
-            # app.run()
-            # hack fix for prman failling to compile shaders after running sam!
-            pipe.apps.prman().expand()
-
+            if float(os.environ["MAYA_VERSION"]) <= 2017:
+                if pipe.isEnable('prman'):
+                    pipe.apps.prman().expand()
             # import IECore;import Gaffer;import GafferUI;IECore.ClassLoader.defaultLoader( "GAFFER_APP_PATHS" ).load( 'opa' )().run()
 
         def __publish(f='particle', t='nParticles') :
@@ -301,103 +282,29 @@ def loadAssetManager():
             a.parameters()['action'] = 'publish'
             a.run()
 
-            #
-            # import maya.cmds as m
-            # import IECore
-            # import Gaffer
-            # import PySide
-            # import os, pipe
-            # os.environ['PIPE_PUBLISH_FILTER'] = f
-            # appLoader = IECore.ClassLoader.defaultLoader( "GAFFER_APP_PATHS" )
-            # # appLoader.classNames()
-            # app=appLoader.load( 'opa' )()
-            # app.parameters()['arguments'] = IECore.StringVectorData(['-Asset.type','%s/%s' % (f,t),'1','IECORE_ASSET_OP_PATHS'])
-            # app.parameters()['op'] = 'publish'
-            # app.parameters()['gui'] = 1
-            # app.run()
-
-
-
         def __gather() :
             import IECore
             c = IECore.ClassLoader.defaultLoader( "GAFFER_APP_PATHS" )
             a = c.load( "sam" )()
             a.run()
 
-            # def sam():
-            #     global _samWindow
-            #     import IECore
-            #     import Gaffer
-            #     import GafferUI
-            #     import samEditor
-            #     import assetBrowser
-            #     if '_samWindow' not in globals().keys():
-            #         with GafferUI.Window( "SAM" ) as _samWindow:
-            #                 browser = samEditor.SamEditor( Gaffer.ScriptNode() )
-            #         GafferUI.EventLoop.mainEventLoop().start()
-            #     _samWindow._qtWidget().resize( 1800, 800 )
-            #     _samWindow.setVisible( True )
-            #
-            # sam()
-            # global sam_window
-            # import IECore
-            # import Gaffer
-            # import GafferUI
-            # import os, pipe
-            # import samEditor
-            # import assetBrowser
-            # scriptNode = Gaffer.ScriptNode()
-            # with GafferUI.Window( "SAM" ) as sam_window  :
-            #         browser = samEditor.SamEditor( scriptNode )
-            # browser.pathChooser().getPath().setFromString( '/' )
-            # sam_window._qtWidget().resize( 1800, 800 )
-            # sam_window.setVisible( True )
-            # GafferUI.EventLoop.mainEventLoop().start()
-
-
-
         import assetListWidget
         if not m.about(batch=1):
-            menu = IECore.MenuDefinition()
-            # menu.append("/Publish/Render",                 {"command" : __publishRender,"active" : True})
-            # menu.append("/Publish/RenderSettings",         {"command" : lambda: __publish('renderSettings', 'maya'), "active" : True})
-            # menu.append("/Publish/Camera",                 {"command" : lambda: __publish('camera', 'alembic'), "active" : True})
-            # menu.append("/Publish/Lighting",               {"command" : lambda: __publish('lighting', 'maya'), "active" : True})
-            # menu.append("/Publish/Rigging",                {"command" : lambda: __publish('rigging', 'maya'), "active" : True})
-            # menu.append("/Publish/Shaders",                {"command" : lambda: __publish('shaders', 'maya'), "active" : True})
-            # menu.append("/Publish/Model",                  {"command" : lambda: __publish('model', 'alembic'), "active" : True})
-            # # menu.append("/Publish/Model/Maya",             {"command" : lambda: __publish('model', 'maya'), "active" : True})
-            # menu.append("/Publish/Animation/Alembic",      {"command" : lambda: __publish('animation', 'alembic'), "active" : True})
-            # menu.append("/Publish/Animation/Maya",         {"command" : lambda: __publish('animation', 'alembic'), "active" : True})
-            # menu.append("/Publish/nParticle",              {"command" : lambda: __publish('particle', 'nParticles'), "active" : True})
-            # # menu.append("/Publish/Rig/skeleton",         {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Rig/light",            {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Camera/Maya",            {"command" : lambda: __publish('camera', 'maya'), "active" : True})
-            # # menu.append("/Publish/Animation/camera",     {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Animation/vertex",     {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Animation/skeleton",   {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Animation/particles",  {"command" : __gather, "active" : False})
-            # # menu.append("/Publish/Texture",              {"command" : __gather, "active" : False})
-            # # for each in Asset.types():
-            # #     s = each.split('/')
-            # #     menu.append("/Publish/%12s/%s" % (s[0],s[1]),      {"command" : lambda: __publish(s[0], s[1]), "active" : True})
-
-            import assetListWidget
+            # if float(os.environ["MAYA_VERSION"]) <= 2017:
             assetListWidget.SAMPanelUI()
+            menu = IECore.MenuDefinition()
             assetListWidget.SAMShelf()
 
-            menu.append("/SAM Browser",                 {"command" : __gather, "active" : True, "description" : "Open the SAM Browser"})
             menu.append("/SAM Panel",                   {"command" : "import assetListWidget;reload(assetListWidget);assetListWidget.SAMPanelUI()", "active" : True, "description" : "Refresh and Open the SAM Panel"})
+            menu.append("/SAM Browser",                 {"command" : __gather, "active" : True, "description" : "Open the SAM Browser"})
             menu.append("-----",                          {"divider" : True})
             menu.append("/Recreate SAM Shelf",          {"command" : "import assetListWidget;reload(assetListWidget);assetListWidget.SAMShelf();import pipelineStartup;reload(pipelineStartup)", "active" : True, "description" : "Delete and recreate the SAM Shelf."})
             menu.append("---",                          {"divider" : True})
             menu.append("/Publish is done now from the SAM panel.",{"active" : False, "description" : "leave the mouse over the panel for a few seconds for help!" })
 
-
             # remove if one already exists
             for each in filter( lambda x: m.menu( x, q=1, l=1)=='SAM', m.window( "MayaWindow", query=True, menuArray=True ) ):
                 m.deleteUI( each, menu=True )
-
 
             #create our custom one!
             global __cortexMenuSAM
@@ -412,27 +319,26 @@ def loadAssetManager():
         if not m.about(batch=1):
             atomoShelfs.buildShelf()
 
+    def AESamMenu(arg1, arg2):
+        AEmenu = meval('global string $gAEMenuBarLayoutName;$__samAEMenu=$gAEMenuBarLayoutName')
+        m.setParent( AEmenu )
+        m.setParent( "AESamMenu", menu=1 )
+        m.menu( "AESamMenu", edit=1, deleteAllItems=1 )
+        m.menuItem( divider=1 )
+        m.ls(sl=1)
+        node = meval('global string $gAECurrentTab;$__samAEMenuSelected=$gAECurrentTab')
+        if node:
+            if m.nodeType( node ) == 'shaveHair':
+                m.menuItem( l="Add SAM shave attributes", c=lambda x: AESamShaveNameAttr(node,x) )
+            elif m.nodeType( node ) in ['mesh']:
+                m.menuItem( l="Add SAM Dynamic Rule attribute", c=lambda x: AESamDRAttr(node,x) )
 
-            def AESamMenu(arg1, arg2):
-                AEmenu = meval('global string $gAEMenuBarLayoutName;$__samAEMenu=$gAEMenuBarLayoutName')
-                m.setParent( AEmenu )
-                m.setParent( "AESamMenu", menu=1 )
-                m.menu( "AESamMenu", edit=1, deleteAllItems=1 )
-                m.menuItem( divider=1 )
-                m.ls(sl=1)
-                node = meval('global string $gAECurrentTab;$__samAEMenuSelected=$gAECurrentTab')
-                if node:
-                    if m.nodeType( node ) == 'shaveHair':
-                        m.menuItem( l="Add SAM shave attributes", c=lambda x: AESamShaveNameAttr(node,x) )
-                    elif m.nodeType( node ) in ['mesh']:
-                        m.menuItem( l="Add SAM Dynamic Rule attribute", c=lambda x: AESamDRAttr(node,x) )
-
-            # add SAM menu to attribute editor
-            AEmenu = meval('global string $gAEMenuBarLayoutName;$__samAEMenu=$gAEMenuBarLayoutName')
-            m.setParent( AEmenu )
-            if "AESamMenu" not in m.menuBarLayout( AEmenu, q=1, menuArray=1 ):
-                m.menu("AESamMenu", label="SAM", pmc=AESamMenu)
-            m.menu("AESamMenu", e=1, pmc=AESamMenu)
+    # add SAM menu to attribute editor
+    AEmenu = meval('global string $gAEMenuBarLayoutName;$__samAEMenu=$gAEMenuBarLayoutName')
+    m.setParent( AEmenu )
+    if "AESamMenu" not in m.menuBarLayout( AEmenu, q=1, menuArray=1 ):
+        m.menu("AESamMenu", label="SAM", pmc=AESamMenu)
+    m.menu("AESamMenu", e=1, pmc=AESamMenu)
 
 
     print 'loadAssetManager() done: %.02f secs' % (time()-startTime)
@@ -465,24 +371,29 @@ def RMS_setup():
 # m.scriptJob( runOnce=True,  idleEvent=loadAssetManager )
 # m.scriptJob( runOnce=True,  idleEvent=RMS_setup )
 if m.about(batch=1):
-    pipeIdleStartup()
-    loadAssetManager()
+    # pipeIdleStartup()
+    # loadAssetManager()
     if float(os.environ["MAYA_VERSION"]) < 2018:
-        RMS_setup()
+        if pipe.isEnable('prman'):
+            RMS_setup()
 else:
     def __runAll__():
         # if float(os.environ["MAYA_VERSION"]) < 2018:
+            import maya.OpenMayaUI as omui
+            print omui.MQtUtil.getCurrentParent()
+            import time
             import genericAsset
-            pb = genericAsset.progressBar(3,"Finishing maya startup... ")
-            pb.step()
+            # pb = genericAsset.progressBar(3,"Finishing maya startup... ")
+            # pb.step()
             pipeIdleStartup()
-            pb.step()
+            # pb.step()
             loadAssetManager()
-            pb.step()
-            print float(os.environ["MAYA_VERSION"])
+            # pb.step()
+            # print float(os.environ["MAYA_VERSION"])
             if float(os.environ["MAYA_VERSION"]) < 2018:
-                RMS_setup()
-            pb.close()
+                if pipe.isEnable('prman'):
+                    RMS_setup()
+            # pb.close()
     # __runAll__()
     m.scriptJob( runOnce=True,  idleEvent=__runAll__ )
 
