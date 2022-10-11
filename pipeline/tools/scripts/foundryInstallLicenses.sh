01982537014e4633001b56752cb30034a02d9032
rhradec <me@hradec.com>
2022-10-11 14:40:21 -0700

    tested and successfully install foundry licenses now.

--- pipeline/tools/scripts/foundryInstallLicenses.sh
+++ pipeline/tools/scripts/foundryInstallLicenses.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+run() {
+	echo "$@"
+	$@
+}
+
 cd /tmp
 zip="$HOME/Downloads/FoundryLicenseKeys.zip"
+if [ "$1" != "" ] ; then
+	zip="$1"
+fi
 
 if [ -e $zip ] ; then
-	for n in 245 246 247 ; do
-		unzip -p $zip | sed 's/.server_name./localhost/g' > ./foundry.lic
-		echo rsync -avpP ./foundry.lic root@192.168.0.245:/usr/local/foundry/RLM/ 
-		echo ssh root@192.168.0.245 reboot
+	for n in 245 246 ; do #247 ; do
+		unzip -p $zip | sed 's/.server_name./localhost/g' > /dev/shm/foundry.lic
+		run rsync -avpP /dev/shm/foundry.lic root@192.168.0.$n:/root/rlm/foundry.lic
+		run ssh root@192.168.0.$n "/bin/bash -c \"pkill -fc -9 rlm ; sleep 15 ; ps -AHfc | grep rlm\"" &
 	done
-	rm $zip
+	wait
+	echo rm $zip
 fi
