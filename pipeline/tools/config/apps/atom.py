a999342e50c4dcdf5102f2efc920df5faf75a46e
rhradec <me@hradec.com>
2022-10-21 21:59:19 -0700

    fixed atom to run on fedora with virtuagl

--- pipeline/tools/config/apps/atom.py
+++ pipeline/tools/config/apps/atom.py
@@ -21,9 +21,36 @@
 
 class atom(baseApp):
 
+    def macfix(self, macfixData):
+        ''' overrides the root path
+        this is called before everything else, including self.environ() '''
+
+        if os.path.exists(self.path("usr")):
+            # starting on version 1.60, atom folder is just the uncompressed .deb
+            # file. So we choose to keep the folder structure found in the .deb
+            # which starts at /usr, /opt, etc.
+            macfixData['subpath'] = 'usr/'
+
     def environ(self):
+        ''' sets the environ to run the app '''
         # fix for: symbol lookup error: /usr/lib/libfontconfig.so.1: undefined symbol: FT_Done_MM_Var
         self.ignorePipeLib( "freetype" )
+
+        # prevent from setting pipeVFX python environment
+        self.ignorePipeLib( "python" )
+
+        # atom needs internet, so we set the proxy here for it
+        self['http_proxy'] = os.environ['PIPE_PROXY_SERVER']
+        self['https_proxy'] = os.environ['PIPE_PROXY_SERVER']
+        self['ftp_proxy'] = os.environ['PIPE_PROXY_SERVER']
+
+    def run(self, app):
+        ''' override the command line
+        this is called right before running the "app" command line,
+        when all os.environ variables are set and ready. '''
+        # os.system( '%s/bin/apm config set http-proxy %s' % (self.path(), os.environ['PIPE_PROXY_SERVER']) )
+        # os.system( '%s/bin/apm config set https-proxy %s' % (self.path(), os.environ['PIPE_PROXY_SERVER']) )
+        baseApp.run( self, app+' --no-sandbox --proxy-server="%s"' %  os.environ['PIPE_PROXY_SERVER'] )
 
     def bg(self, cmd, bin):
         ''' return True if a cmd or binary should run in background '''
