061d05e5ff1a032574bad028505450302617d009
rhradec <me@hradec.com>
2022-10-21 22:05:33 -0700

    python3 fixes

--- pipeline/tools/python/build/__init__.py
+++ pipeline/tools/python/build/__init__.py
@@ -17,10 +17,12 @@
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with pipeVFX.  If not, see <http://www.gnu.org/licenses/>.
 # =================================================================================
-
+import sys, os
+sys.path.insert( 0, os.path.abspath( os.path.dirname(__file__) ) )
 from devRoot import *
 from ftp import *
 from genericBuilders import *
+from genericBuilders import versionMajor
 from mayaPlugin import *
 from mayaPython import *
 from mayaPythonPlugin import *
