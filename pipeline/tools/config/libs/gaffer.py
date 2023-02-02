536a9a6ef92558e8c7d92e38990037546ce310f6
rhradec <me@hradec.com>
2023-02-01 18:13:33 -0800

    added postrun frame check like in maya, version dependency fixing

--- pipeline/tools/config/libs/gaffer.py
+++ pipeline/tools/config/libs/gaffer.py
@@ -22,13 +22,26 @@ class gaffer(baseLib):
     def versions(self):
         ''' set the pipe python version according to houdinis python version '''
         if self.parent() in ['gaffer']:
-            gv = float('.'.join(pipe.libs.version.get( 'gaffer' ).split('.')[:2]))
-            if gv >= 1.1:
-                pipe.version.set( python = '3.9' )
-                pipe.libs.version.set( python = '3.9' )
+            gv = pipe.libs.version.get( 'gaffer' )
+            if pipe.versionBiggerEqualThan( gv, '1.1.0.0'):
+                # default libraries set
+                pipe.version.set( python = '3.9.7' )
+                pipe.libs.version.set( python = '3.9.7' )
+                pipe.libs.version.set( cortex = '10.4.2.1' )
+                # if self.parent in ['maya']:
+                #     pipe.version.set( python = '3.9.7' )
+                #     pipe.libs.version.set( python = '3.9.7' )
+                # else:
+                #     pipe.version.set( python = '3.7.5' )
+                #     pipe.libs.version.set( python = '3.7.5' )
                 pipe.libs.version.set( openexr = '2.4.1' )
                 pipe.libs.version.set( usd = '21.11.1' )
                 pipe.libs.version.set( usd_non_monolithic = '21.11.1' )
+                pipe.libs.version.set( boost = '1.76.0' )
+                if pipe.versionBiggerEqualThan( gv, '1.1.7.0'):
+                    pipe.apps.version.set( arnold = '7.1.4.1' )
+                else:
+                    pipe.apps.version.set( arnold = '7.1.3.2' )
             else:
                 pipe.version.set( python = '2.7' )
                 pipe.libs.version.set( python = '2.7' )
@@ -49,7 +62,7 @@ class gaffer(baseLib):
 
         self['PYTHONPATH'] = self.path('python')
 
-        # if self.parent() not in ['maya']:
+                # if self.parent() not in ['maya']:
         #     # gaffer can't inherit it's env vars from a call app, so we're forced to
         #     # clean then up here from os.environ to make sure!
         #     for each in filter(lambda x: 'GAFFER' in x, os.environ.keys()):
@@ -59,6 +72,9 @@ class gaffer(baseLib):
         self.update( pipe.libs.ocio() )
         self.update( pipe.libs.cuda() )
         self.update( pipe.libs.cycles() )
+
+        # if hasattr( pipe.apps, 'LDTGaffer' ):
+        #     self.update( pipe.apps.LDTGaffer() )
 
         # add all versions of OIIO libraries to search path
         if hasattr(pipe.libs, 'oiio'):
@@ -72,6 +88,9 @@ class gaffer(baseLib):
         # else:
         #     # any other version (old gaffer), uses PyQt4
         #     self['GAFFERUI_QT_BINDINGS'] = 'PyQt4'
+
+        if self.parent() in ['gaffer']:
+            self['PYTHONHOME'] = pipe.libs.python().path()
 
         if self.parent() in ['maya']:
             if int(pipe.apps.version.get('maya').split('.')[0]) < 2014:
@@ -91,6 +110,7 @@ class gaffer(baseLib):
                 ],
                 scripts = [
                     '%s/gaffer/python' % each,
+                    '%s/gaffer/python/nodes' % each,
                 ],
                 startups = [
                     '%s/gaffer/startup' % each,
@@ -171,26 +191,127 @@ class gaffer(baseLib):
 
             # set jemalloc for gaffer
             self['GAFFER_JEMALLOC'] = '0'
-            self['LD_PRELOAD'] = pipe.libs.jemalloc().LD_PRELOAD()
+            # self['LD_PRELOAD'] = pipe.libs.jemalloc().LD_PRELOAD()
 
         # to enable cycles
-        self['GAFFERCYCLES_FEATURE_PREVIEW'] = '1'
-        # $CYCLES_ROOT/$(ldd  $GAFFER_ROOT/lib/libGafferUSD.so  | grep -i usd | awk '{print $(NF-1)}' | awk -F'/' '{print $(NF-2)"-usd."$(NF-3)"-gaffer."}' | sort | uniq)$GAFFER_VERSION/
-        self['CYCLES_TARGET_FOLDER'] = ''.join([
-            pipe.libs.cycles().path(),
-            '/',
-            cached.popen('''ldd  %s/lib/libGafferUSD.so  | grep -i usd | awk '{print $(NF-1)}' | awk -F'/' '{print $(NF-2)"-usd."$(NF-3)"-gaffer."}' | sort | uniq''' % self.path()).readlines()[0].strip(),
-            self.version().strip(),
-        ]).replace(' ','').strip()
+        if self.path('lib/libGafferUSD.so'):
+            self['GAFFERCYCLES_FEATURE_PREVIEW'] = '1'
+            # $CYCLES_ROOT/$(ldd  $GAFFER_ROOT/lib/libGafferUSD.so  | grep -i usd | awk '{print $(NF-1)}' | awk -F'/' '{print $(NF-2)"-usd."$(NF-3)"-gaffer."}' | sort | uniq)$GAFFER_VERSION/
+            self['CYCLES_TARGET_FOLDER'] = ''.join([
+                pipe.libs.cycles().path(),
+                '/',
+                cached.popen('''ldd  %s  | grep -i usd | awk '{print $(NF-1)}' | awk -F'/' '{print $(NF-2)"-usd."$(NF-3)"-gaffer."}' | sort | uniq''' % self.path('lib/libGafferUSD.so')).readlines()[0].strip(),
+                self.version().strip(),
+            ]).replace(' ','').strip()
+            self['GAFFERCYCLES'] = '1'
+
+        # self['LD_PRELOAD'] = pipe.LD_PRELOAD.latestGCCLibrary("libstdc++.so.6")
+        # self['LD_PRELOAD'] = pipe.LD_PRELOAD.latestGCCLibrary("libgcc_s.so.1")
+
+        # workaround to avoid loading mesa llvm
+        self['LIBGL_ALWAYS_INDIRECT'] = '1'
+
 
 
     def preRun(self, cmd):
         os.environ['CYCLES_ROOT'] = self['CYCLES_TARGET_FOLDER']
-        return cmd
+        # # workaround to avoid loading mesa llvm
+        # os.environ['LIBGL_ALWAYS_INDIRECT'] = '1'
 
-    # def runUserSetup(self, bin):
-    #     ''' only create a user folder structure if it's the main gaffer app.'''
-    #     return bin[0] in ['gaffer', 'bundle']
+        # os.environ['PATH'] = self.path('bin')+':'+os.environ['PATH']
+
+
+        # if running the gaffer wrapper, add /bin/bash to the command line
+        # this fixes running with gdb!
+        new = []
+        for each in cmd.split(' '):
+            if 'bin/gaffer' in each[-12:]:
+                new += ['/bin/bash']
+            new += [each]
+        return ' '.join(new)
+
+
+    def postRun(self, cmd, ret, returnLog=''):
+        import glob
+        images = []
+        error = False
+        logError = False
+
+        if 'execute' in cmd:
+            # grab images from log for checking
+            if 'writing file' in returnLog:
+                # collect image files from output log
+                images += [ z.split('writing file')[-1].replace('`','').replace("'",'').strip() for z in returnLog.split('\n') if 'writing file' in z ]
+
+                # run our pipe.frame.check generic frame check for the gathered image list
+                if images:
+                    error = pipe.frame.check( images )
+
+                    # move rendered frames to asset, if this render is an asset!
+                    if not error:
+                        if not self.asset:
+                            # If we dont have an asset, try figure it out from the maya scene path
+                            # find maya scene file
+                            assetPath = None
+                            for each in cmd.split(' '):
+                                each = each.replace('"','')
+                                if os.path.splitext(each.lower())[-1] in ['.mb','.ma']:
+                                    assetPath = each
+                                    break
+                            if assetPath:
+                                pipe.frame.publish( images, assetPath ) #, returnLog )
+                        else:
+                            # if we have an asset reference already, use it!
+                            pipe.frame.publish( images, self.asset )
+
+        # if images are broken, fail right away
+        if  error:
+            print('ERROR: At least one of the rendered images is broken. Render failed!' % ret)
+            ret = -1
+
+        # if no images or images are ok, check log for errors
+        else:
+            # only fail if no images where generated
+            if not images:
+                errors = [
+                ]
+                if returnLog:
+                    for s in errors:
+                        if s in str(returnLog):
+                            logError = True
+                            ret = -1
+                            for line in [ x for x in str(returnLog).split('\n') if s in x ]:
+                                print( '[ SOFT   ERROR ] - caused by: "'+line+'"' )
+                            break
+
+            # fatal errors - must fail even if images were generated!!
+            # theses errors cause the rendered image to be wrong.
+            fatalErrors = [
+                'ERROR : gaffer execute',
+            ]
+            for s in fatalErrors:
+                if s in str(returnLog):
+                    logError = True
+                    ret = -1
+                    for line in [ x for x in str(returnLog).split('\n') if s in x ]:
+                        print( '[ FATAL  ERROR ] - caused by: "'+line+'"' )
+                    break
+
+        # if no logError, and running gaffer gui, throw a WARNING informing
+        # a wrong segfault, but return 0
+        if self.parent() in ['gaffer'] and not logError:
+            if ret != 0:
+                print('WARNING: Gaffer exited with error code %d, but due to some segfaults caused by Arnold, we have to assume return code 0 or else renders will fail!' % ret)
+                ret = 0
+
+        return ret
+
+    def runUserSetup(self, bin):
+        ''' only create a user folder structure if it's the main gaffer app.'''
+        ret = bin[0] in ['gaffer', 'bundle']
+        if self.parent() in ['gaffer']:
+            ret = True
+        return ret
 
     @staticmethod
     def addon(caller, ops="", procedurals="", apps="", graphics="", scripts="", startups="", shaders="", libs="", extensions=""):
@@ -209,7 +330,12 @@ class gaffer(baseLib):
         if self.path('bin/gaffer'):
             gaffer_bin = 'gaffer'
 
+        # temp hack
         return [
+            # ==================================================================
+            # temp HACK disabling gaffer alias in favor of gaffer wrapper
+            # script, to set pipevfx to 5.0.1 until we fix maya 2023 gaffer
+            # ==================================================================
             ['gaffer',  gaffer_bin],
             ['opa',     gaffer_bin+' opa -gui 1'],
             ['browser', gaffer_bin+' browser'],
